b7a14e1c2681a8f706b1673afd3d1be837b6e13d
Tomohiko Mukai <contact@mukai-lab.org>
2018-10-22 17:08:22 +0900

    第5回演習課題(仮)の追加

---
+++ 05-ex.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // 20個の乱数を格納した配列
+    int array[20];
+    for (int i = 0; i < 20; ++i)
+    {
+        array[i] = rand();
+    }
+    
+    // TODO: 以降，必要なコードを追加
+    
+}
