0d263fe89157e5ba7769751c2e77b2c3c9883af6
Tomohiko Mukai <contact@mukai-lab.org>
2018-11-06 11:08:29 +0900

    第7回課題ファイルの追加

---
+++ 07-01.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 2つの整数値を小さい順に並び替える
+// 関数 myswap をポインタを引数として作成
+
+int main(void)
+{
+    int a[5] = {0, 1, 2, 3, 4};
+    int b[5] = {5, 4, 3, 2, 1};
+    
+    for (int i = 0; i < 5; ++i)
+    {
+        // myswap を呼び出して a[i] と b[i] を並び替え
+
+        cout << a[i] << ", " << b[i] << endl;
+    }
+    return 0;
+}
---
+++ 07-02.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// sincos関数を実装
+
+int main(void)
+{
+    for (int angle = 0; angle <= 360; ++angle)
+    {
+        // ラディアンに変換（計算式を追加）
+        double radian = 
+        
+        double sv = 0, cv = 0;
+        sincos(sv, cv, radian);
+
+        cout << sv << ", " << cv << endl;
+    }
+    return 0;
+}
