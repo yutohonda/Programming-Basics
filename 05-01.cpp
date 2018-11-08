dd794ffabbd59556313a5c0abab970aadec9f340
Tomohiko Mukai <contact@mukai-lab.org>
2018-11-08 22:11:33 +0900

    第5回課題解答例

--- 05-01.cpp
+++ 05-01.cpp
@@ -4,4 +4,28 @@ using namespace std;
 
 int main()
 {
+    for (int i = 0; i < 5; ++i) // 中括弧{ }内の処理を5回繰り返す
+    {
+        double x;
+        cin >> x;                // データを受け取って
+        cout << x * 2.0 << endl; // 2倍して出力する
+    }
+}
+
+// 別解
+int main()
+{
+    double data[5];
+    for (int i = 0; i < 5; ++i)
+    {
+        cin >> data[i];          // 配列の各要素に先頭から順に入力
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+        data[i] = data[i] * 2.0; // 配列の各要素を先頭から順に2倍
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << data[i] << endl; // 配列の各要素を先頭から順に出力
+    }
 }
--- 05-ex.cpp
+++ 05-ex.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <algorithm> // 解2の場合に必要
 
 using namespace std;
 
@@ -13,5 +14,26 @@ int main()
     }
     
     // TODO: 以降，必要なコードを追加
+
+    // 解1. バブルソート
+    for (int i = 0; i < 19; ++i)
+    {
+        for (int j = i + 1; j < 20; ++j)
+        {
+            if (array[i] > array[j])
+            {
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+            }
+        }
+    }
     
+    // 解2. STLのsort
+    sort(array, array + 20);
+    
+    for (int i = 0; i < 20; ++i)
+    {
+        cout << array[i] << endl;
+    }
 }
