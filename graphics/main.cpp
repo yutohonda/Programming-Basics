e36dc1f99f2f13e7b694901cb730ccb3046a2737
Tomohiko Mukai <tmki@acm.org>
2018-11-15 22:46:07 +0900

    数値表現などを修正

--- graphics/main.cpp
+++ graphics/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
 #ifdef WIN32
 #include <windows.h>
 #include <gl/GL.h>
@@ -24,9 +23,9 @@ void postdisplay();    // 描画終了時に必ず呼ぶ関数
 void display()
 {
     predisplay(); // 描画開始時に必ず呼ぶ関数。消してはならない
-
     /***** 以降を編集する *****/
 
+    // キーボード入力に応じた処理
     if (key == 'q' || key == 'Q')
     {
         exit(0);
@@ -65,7 +64,7 @@ void display()
     float teapot1Color[4] = { 1.0, 0.2, 0.2, 1.0 };
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, teapot1Color);
     // ティーポットの描画。引数には大きさを指定
-    glutSolidTeapot(10.0);
+    glutSolidTeapot(20.0);
     //
     glPopMatrix();
 
@@ -74,13 +73,12 @@ void display()
     double px = 80.0 * sin(time * 2.0);
     glTranslated(px, 0, 0);
     glRotated(time * 100.0, 0, 1.0, 0);
-    float teapot2Color[4] = { 0.2, 0.2, 1.0, 0.5f };
+    float teapot2Color[4] = { 0.2, 0.2, 1.0, 0.5 };
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, teapot2Color);
-    glutSolidTorus(5.0f, 10.0f, 50, 50); // 引数には2種類の半径と、あとは適当に50を2つ
+    glutSolidTorus(5.0, 20.0, 50, 50); // 引数には輪の太さと環の大きさ、あとは適当に50を2つ
     glPopMatrix();
 
     /***** ここまで編集する *****/
-
     postdisplay(); // 描画終了時に必ず呼ぶ関数。消してはならない
 }
 
@@ -90,13 +88,13 @@ void display()
 void predisplay()
 {
     // 背景色の設定
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 背景色 [Red, Green, Blue, Alpha] Alphaは1.0固定
+    glClearColor(0.0, 0.0, 0.0, 1.0); // 背景色 [Red, Green, Blue, Alpha] Alphaは1.0固定
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(0, 0, 100.0,
-        0, 0, 0,
-        0, 1.0, 0.0);
+              0, 0, 0,
+              0, 1.0, 0.0);
 }
 
 void postdisplay()
@@ -150,7 +148,7 @@ void resize(int w, int h)
 
 void mouseMotion(int x, int y)
 {
-    mousePosX = (x - screenWidth / 2) * 200.0 / screenWidth;
+    mousePosX =  (x - screenWidth  / 2) * 200.0 / screenWidth;
     mousePosY = -(y - screenHeight / 2) * 200.0 / screenHeight;
     glutIdleFunc(idle);
 }
@@ -158,9 +156,8 @@ void mouseMotion(int x, int y)
 int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-
     glutInitWindowSize(800, 800); // ウィンドウサイズ [幅, 高さ]
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     glutCreateWindow("graphics"); // ウィンドウ名
     
     glutDisplayFunc(display);
@@ -168,23 +165,21 @@ int main(int argc, char* argv[])
     glutKeyboardFunc(keyboard);
     glutPassiveMotionFunc(mouseMotion);
     glutReshapeFunc(resize);
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_NORMALIZE);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_LIGHT0);
-    float modelSpecular[4] = { 0.5f, 0.5f, 0.5f, 1.0f };
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, modelSpecular);
-    float black[4] = { 0.0, 0.0, 0.0, 1.0 };
+    float gray[4]  = { 0.5, 0.5, 0.5, 1.0 };
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, gray);
+    float black[4] = { 0.1, 0.1, 0.1, 1.0 };
     glLightfv(GL_LIGHT0, GL_AMBIENT, black);
 
-    // ライトの方向 [x, y, z, 1]
-    float lightDir[4] = { 0, 1.0f, 1.0f, 0.0f };
-    glLightfv(GL_LIGHT0, GL_POSITION, lightDir);
+    // ライトの位置 [x, y, z, 1]
+    float lightPos[4] = { 0, 80.0, 100.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
     // ライトの色 [Red, Green, Blue, 1]
-    float lightColor[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+    float lightColor[4] = { 1.0, 1.0, 1.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  lightColor);
     glLightfv(GL_LIGHT0, GL_SPECULAR, lightColor);
 
 #ifdef WIN32
