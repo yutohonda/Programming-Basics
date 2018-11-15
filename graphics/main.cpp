b4e4c94cfad98e26977e2991282dcda2229a5ab0
Tomohiko Mukai <contact@mukai-lab.org>
2018-11-15 13:13:12 +0900

    コメントの追加

--- graphics/main.cpp
+++ graphics/main.cpp
@@ -12,18 +12,20 @@
 
 using namespace std;
 
-double getTime();
-double mousePosX = 0;
-double mousePosY = 0;
-unsigned char key = 0;
-void predisplay();
-void postdisplay();
+double getTime();      // 開始時からの経過時間を秒単位で取得する関数
+double mousePosX = 0;  // マウスカーソル位置X
+double mousePosY = 0;  // マウスカーソル位置Y
+unsigned char key = 0; // キーボードからの入力値
+void predisplay();     // 描画開始時に必ず呼ぶ関数
+void postdisplay();    // 描画終了時に必ず呼ぶ関数
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void display()
 {
-    predisplay(); // 消してはならない
+    predisplay(); // 描画開始時に必ず呼ぶ関数。消してはならない
+
+    /***** 以降を編集する *****/
 
     if (key == 'q' || key == 'Q')
     {
@@ -33,46 +35,66 @@ void display()
     double time = getTime(); // 経過時間を秒単位で取得
 
     // 画面を横切る線
-    glLineWidth(1);
-    float lineColor[4] = { 1.0, 1.0, 1.0, 1.0 };
+    glPushMatrix();
+    //
+    // 引数に線の太さを指定（浮動小数）
+    glLineWidth(1.0);
+    // 線の色：4次元配列の先頭から[Red, Green, Blue, Alpha] を指定。Alphaは透明度
+    float lineColor[4] = { 1.0, 1.0, 1.0, 1.0 }; 
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, lineColor);
-    glBegin(GL_LINES);
-    glVertex3d(-100.0, 0, 0);
-    glVertex3d(100.0, 0, 0);
-    glEnd();
+    // 線の描画
+    glBegin(GL_LINES);        // 線描はじめ
+    glVertex3d(-100.0, 0, 0); // 始端 [x, y, z]
+    glVertex3d(100.0, 0, 0);  // 終端 [x, y, z]
+    glEnd();                  // 線描おわり
+    //
+    glPopMatrix();
 
     // マウスカーソルについてくるティーポット
     glPushMatrix();
+    //
+    // ティーポットの位置指定 [x, y, z]
+    glTranslatef(mousePosX, mousePosY, 2);
+    // ティーポットの回転 [回転角度(degree), 回転軸x, 回転軸y, 回転軸z]
+    glRotatef(time * 60.0f, 0, 0, 1.0f);
+    // ティーポットの色
     float color1[4] = { 1.0, 0.5, 0.5, 1.0 };
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color1);
-    glTranslatef(mousePosX, mousePosY, 0);
-    glRotatef(time * 60.0f, 0, 0, 1.0f);
+    // ティーポットの描画。引数には大きさを指定
     glutSolidTeapot(10.0);
+    //
     glPopMatrix();
 
-    // 画面を横方向に往復しているティーポット
+    // 画面を横方向に往復しているティーポット（同上）
     glPushMatrix();
+    //
     double px = 80.0 * sin(time * 2.0);
     glTranslatef(px, 0, 0);
-    float color2[4] = { 0.5, 0.5, 1.0, 1.0 };
+    float color2[4] = { 0.5, 0.5, 1.0, 0.5 };
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color2);
     glutSolidTeapot(10.0);
+    //
     glPopMatrix();
 
-    postdisplay(); // 消してはならない
+    /***** ここまで編集する *****/
+
+    postdisplay(); // 描画終了時に必ず呼ぶ関数。消してはならない
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
+#pragma region not have to edit
 void predisplay()
 {
+    // 背景色の設定
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // 背景色 [Red, Green, Blue, Alpha] Alphaは1.0固定
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0, 0, 130.0,
-        0, 0, 0,
-        0, 1.0, 0.0);
+    // カメラの設定
+    gluLookAt(0, 0, 130.0,  // カメラの中心位置 [x, y, z]
+              0, 0, 0,      // カメラが注視する位置 [x, y, z]
+              0, 1.0, 0.0); // [0, 1, 0] で固定（google: CG y-up）
 }
 
 void postdisplay()
@@ -86,6 +108,7 @@ void keyboard(unsigned char k, int x, int y)
     key = k;
 }
 
+static double startTime;
 static double prevTime;
 double getTime()
 {
@@ -93,12 +116,11 @@ double getTime()
     LARGE_INTEGER freq, t;
     QueryPerformanceFrequency(&freq);
     QueryPerformanceCounter(&t);
-    return (double)t.QuadPart / freq.QuadPart;
+    return (double)t.QuadPart / freq.QuadPart - startTime;
 #else
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
-    uint64_t absoluteUs =
-        return ts.tv_sec + ts.tv_nsec) * 1.0e3;
+    return ts.tv_sec + ts.tv_nsec / 1.0e9 - startTime;
 #endif
 }
 
@@ -147,7 +169,21 @@ int main(int argc, char* argv[])
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+
+#ifdef WIN32
+    LARGE_INTEGER freq, t;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&t);
+    startTime = (double)t.QuadPart / freq.QuadPart;
+#else
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    startTime = ts.tv_sec + ts.tv_nsec / 1.0e9;
+#endif
     prevTime = getTime();
     glutMainLoop();
     return 0;
 }
+#pragma endregion
