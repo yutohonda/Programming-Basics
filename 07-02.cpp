21984255c2233816e8a8468efdba82b8d6e0f013
yutohonda <43771570+yutohonda@users.noreply.github.com>
2018-11-19 02:10:43 +0900

    Update 07-02.cpp

--- 07-02.cpp
+++ 07-02.cpp
@@ -1,16 +1,22 @@
-﻿#include <iostream>
+#include <iostream>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 // sincos関数を実装
+void sincos(double &s, double &c, double radian )
+{
+    s = sin(radian);
+    c = cos(rdian);
+}
 
 int main(void)
 {
     for (int angle = 0; angle <= 360; ++angle)
     {
         // ラディアンに変換（計算式を追加）
-        double radian = 
+        double radian = angle * 3.14 / 180
         
         double sv = 0, cv = 0;
         sincos(sv, cv, radian);
